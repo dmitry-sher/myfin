7a6322e7cefafa5b2f27c62c79cebb75aaf86a3a
Dmitry Sher <dima.virtuallight@gmail.com>
2025-05-11 21:28:25 +0200

    add full plan analytics

--- src/components/PlanHeader.tsx
+++ src/components/PlanHeader.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from "react";
 
+import { MonthTotalsContext } from "../context/MonthTotalsContext";
 import { Plan, Transaction } from "../types/entities";
 
+import { ViewPieChartButton } from "./TransactionMonthButtons/ViewPieChartButton";
+import { ViewTotalsButton } from "./TransactionMonthButtons/ViewTotalsButton";
 import { TransactionForm } from "./TransactionForm";
 
 type PlanHeaderProps = {
@@ -21,20 +24,31 @@ export const PlanHeader: FC<PlanHeaderProps> = ({
   }
 
   return (
-    <>
-      <h2 className="text-xl mb-4 mt-4">
-        Plan: {selectedPlan.name}
-        <span className="text-green-600 ml-2">
-          (Balance:{" "}
-          {selectedPlan.transactions.reduce((acc, t) => acc + t.amount, 0)})
-        </span>
-      </h2>
+    <MonthTotalsContext.Provider
+      value={{
+        monthTransactions: selectedPlan.transactions,
+        startingBalance: 0,
+        monthKey: "Full plan",
+      }}
+    >
+      <div className="flex items-center justify-start mb-4 mt-4">
+        <h2 className="text-xl mr-4">
+          Plan: {selectedPlan.name}
+          <span className="text-green-600 ml-2">
+            (Balance:{" "}
+            {selectedPlan.transactions.reduce((acc, t) => acc + t.amount, 0)})
+          </span>
+        </h2>
+
+        <ViewTotalsButton />
+        <ViewPieChartButton />
+      </div>
 
       <TransactionForm
         onAddTransaction={(transaction): void =>
           addTransaction(selectedPlan.id, transaction)
         }
       />
-    </>
+    </MonthTotalsContext.Provider>
   );
 };
