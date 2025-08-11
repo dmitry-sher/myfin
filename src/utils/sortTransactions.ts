d258a3e66f15ca4a1b072f012677e8a7343ccbf0
Dmitry Sher <dima.virtuallight@gmail.com>
2025-08-11 11:01:24 +0200

    fixed sorting inside the day

--- src/utils/sortTransactions.ts
+++ src/utils/sortTransactions.ts
@@ -2,5 +2,9 @@ import { Transaction } from "../types/entities";
 
 export const sortTransactions = (transactions: Transaction[]): Transaction[] =>
   [...transactions].sort((a, b) => {
-    return a.trueDate.getTime() - b.trueDate.getTime();
+    const ret = a.trueDate.getTime() - b.trueDate.getTime();
+    if (ret === 0) {
+      return b.amount - a.amount;
+    }
+    return ret;
   });
